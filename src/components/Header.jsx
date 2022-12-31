f58ccd03dbf6c0ef6758bb7c55dcd6c932287bde
Dunsin <jesudunsinfaiye@gmail.com>
2022-12-31 13:39:34 +0100

    feat: add better height for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='bg-brand flex flex-col p-6 text-white min-h-[5%]'>
-      <h1 className='font-bold mb-[-10px] text-flg'>HearItFresh🎶</h1>
-      <p className='text-fmd'>Discover Fresh Tracks that Fit Your Style</p>
+      <h1 className='font-bold mb-[-6px] text-fmd'>HearItFresh🎶</h1>
+      <p className='text-fsm'>Discover Fresh Tracks that Fit Your Style</p>
     </div>
   )
 }
