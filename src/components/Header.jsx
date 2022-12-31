bde6aad5a475ef7364114e51c7b4a22952dde067
Dunsin <jesudunsinfaiye@gmail.com>
2022-12-31 13:54:34 +0100

    feat: add icon for music

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Icon } from '@iconify/react'
 
 const Header = () => {
   return (
     <div className='bg-brand flex flex-col p-6 text-white min-h-[5%]'>
-      <h1 className='font-bold mb-[-6px] text-fmd'>HearItFresh🎶</h1>
+      <h1 className='font-bold mb-[-6px] text-fmd flex items-center'>HearItFresh<Icon icon="material-symbols:music-note-rounded" width="20" height="20" inline={true} /></h1>
       <p className='text-fsm'>Discover Fresh Tracks that Fit Your Style</p>
     </div>
   )
