0e5ca841ad2dadd1a6b53a2e37db19b9f01a41b6
Dunsin <jesudunsinfaiye@gmail.com>
2023-01-02 20:42:41 +0100

    feat: change condition for displaying result

--- src/components/GetBetterSongs.jsx
+++ src/components/GetBetterSongs.jsx
@@ -255,28 +255,23 @@ const GetBetterSongs = ({ isConnected, logOut }) => {
       </section>
       {!isLoading &&
         <section className='bg-gray-200 md:min-w-[40vw] md:max-w-[50%] min-w-[300px] p-4 rounded'>
-          {playListLink !== null
-            &&
+          {(playListData.link.length !== 0 && playListData.name.length !== 0)
+            ?
             <>
               <div className='text-fmd flex justify-between items-center mb-1'>
-                <div className='text-fsm'>
-                  <p>Check Your Account For The Playlist Or Click On</p>
-                  <p>The Playlist link:</p>
-                </div>
+                <p className='text-fsm'>Check Your Spotify For The Playlist Or</p>
+                <Icon icon="pajamas:copy-to-clipboard" height='18' width='18' color="teal" inline={true} className='cursor-pointer' onClick={copyToClipboard} />
+              </div>
               <a href={playListData.link} target="_blank" rel='norefferer' className='flex gap-1 items-center'>
                 <Icon icon="logos:spotify-icon" width="20" height="20" />
                 <div className='flex flex-col'>
                   <span className='text-fxs font-medium'>Open on Spotify</span>
                   <span className='underline text-brand whitespace-normal text-fxs mt-[-5px]'>{playListData.name}</span>
                 </div>
-              {
-                !(playListLink.length === 0)
-                  ?
-                  <a href={playListLink} target="_blank" rel='norefferer' className='underline text-brand whitespace-normal text-fxs'>{playListLink}</a>
-                  :
-                  <p>Ooops! Something Went Wrong</p>
-              }
+              </a>
             </>
+            :
+            <p className='text-fsm text-red-500'>{isConnected ? buttonClick && 'Ooops! Something Went Wrong' : ''}</p>
           }
 
         </section>}
