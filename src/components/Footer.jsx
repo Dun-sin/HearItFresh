29ff28e0af94c8b3c3e5c7e0b9712f433bc163a9
Dunsin <jesudunsinfaiye@gmail.com>
2023-02-16 09:57:10 +0100

    feat(header): add buy me a coffee button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <div className='h-[5%] p-4 bg-brand text-white text-center flex items-center justify-between'>
       <a href="https://developer.spotify.com/documentation/web-api/" target='_blank' className='font-bold flex flex-wrap items-center underline text-f2xs'>
         Powered By
-        <Icon icon="logos:spotify" width="50" height="50" inline={true} className='ml-1' /></a>
-      <a href="https://github.com/Dun-sin/HearItFresh" className='font-bold underline text-f2xs flex items-center flex-wrap'><Icon icon="typcn:social-github" width="25" height="25" inline={true} />Contribute to the Project</a>
-      <p className='text-f2xs flex gap-1 items-center flex-wrap'>Made With <Icon icon="ri:bear-smile-line" width="20" height="20" inline={true} /> By <a href='https://twitter.com/dunsinWebDev' target='_blank' className='underline font-bold'>Dunsin</a></p>
+        <Icon icon="logos:spotify" width="40" height="40" inline={true} className='ml-1' /></a>
+      <a href="https://github.com/Dun-sin/HearItFresh" className='font-bold underline text-f2xs flex items-center flex-wrap'><Icon icon="typcn:social-github" width="20" height="20" inline={true} />Contribute to the Project</a>
+      <p className='text-f2xs flex items-center justify-center flex-wrap'>Made With <Icon icon="ri:bear-smile-line" width="15" height="15" inline={true} /> By <a href='https://twitter.com/dunsinWebDev' target='_blank' className='underline font-bold'>Dunsin</a></p>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,14 @@ import { Icon } from '@iconify/react'
 
 const Header = () => {
   return (
-    <div className='bg-brand flex flex-col p-6 text-white min-h-[5%]'>
-      <h1 className='font-bold mb-[-6px] text-fmd flex items-center'>HearItFresh<Icon icon="material-symbols:music-note-rounded" width="20" height="20" inline={true} /></h1>
-      <p className='text-fsm'>Discover Fresh Tracks that Fit Your Style</p>
+    <div className='bg-brand flex items-center justify-between p-6 text-white min-h-[5%]'>
+      <div className='flex flex-col'>
+        <h1 className='font-bold mb-[-6px] text-fmd flex items-center'>HearItFresh<Icon icon="material-symbols:music-note-rounded" width="20" height="20" inline={true} /></h1>
+        <p className='text-fsm'>Discover Fresh Tracks that Fit Your Style</p>
+      </div>
+      <a href='https://www.buymeacoffee.com/dunsinCodes' target='_blank' className='bg-yellow-500 h-8 w-28 text-fxs rounded-lg flex items-center justify-center cursor-pointer sm:w-40 font-semibold'>
+        Buy Me A Coffee
+      </a>
     </div>
   )
 }
