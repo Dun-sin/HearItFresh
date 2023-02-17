b65685fc1e369387c741b1632fef758bd8651c03
Gambhir <gambhir786191ss@gmail.com>
2023-02-17 09:40:19 +0530

    Solved the Footer bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const Footer = () => {
   return (
-    <div className='h-[5%] p-4 bg-brand text-white text-center flex items-center justify-between'>
+    <div className='h-[5%] p-4 bg-brand text-white text-center flex items-center justify-between inset-x-0 bottom-0'>
       <a href="https://developer.spotify.com/documentation/web-api/" target='_blank' className='font-bold flex flex-wrap items-center underline text-f2xs'>
         Powered By
         <Icon icon="logos:spotify" width="50" height="50" inline={true} className='ml-1' /></a>
