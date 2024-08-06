51dfeb2e6eac8ca81b175420a7064e188e391862
Dunsin <jesudunsinfaiye@gmail.com>
2024-08-06 10:18:32 +0100

    feat(authUrl): pass authurl to connectspotify

--- app/components/ConnectSpotify.tsx
+++ app/components/ConnectSpotify.tsx
@@ -1,28 +1,27 @@
+'use client';
+
 import { useAuth } from '../context/authContext';
 
-const authUrl = `https://accounts.spotify.com/authorize?client_id=${
-	process.env.SPOTIFY_CLIENT_ID
-}&response_type=code&redirect_uri=${
-	process.env.REDIRECT_URL as string
-}&scope=playlist-modify-public%20playlist-read-private%20user-read-private`;
+const ConnectSpotify = ({ authUrl }: { authUrl: string }) => {
+	const { isAuthInProgress, isLoggedIn } = useAuth();
 
-const ConnectSpotify = () => {
-	const { isAuthInProgress } = useAuth();
 	return (
-		<div className='absolute w-full h-full flex items-center justify-center bg-gray bg-opacity-80 flex-col'>
-			<p className={`text-flg md:text-fmd font-bold`}>
-				{isAuthInProgress
-					? 'Connecting your spotify'
-					: 'Please login to connect your spotify and use this tool'}
-			</p>
-			{!isAuthInProgress && (
-				<a
-					href={authUrl}
-					className='px-4 py-2 bg-brand rounded-md text-lightest'>
-					Connect Your Spotify
-				</a>
-			)}
-		</div>
+		!isLoggedIn && (
+			<div className='absolute w-full h-full flex items-center justify-center bg-gray bg-opacity-80 flex-col'>
+				<p className={`text-flg md:text-fmd font-bold`}>
+					{isAuthInProgress
+						? 'Connecting your spotify'
+						: 'Please login to connect your spotify and use this tool'}
+				</p>
+				{!isAuthInProgress && (
+					<a
+						href={authUrl}
+						className='px-4 py-2 bg-brand rounded-md text-lightest'>
+						Connect Your Spotify
+					</a>
+				)}
+			</div>
+		)
 	);
 };
 
