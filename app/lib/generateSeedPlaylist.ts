4b9aebd705c2747f7b1a9bad4b6d3075fac1dbdd
Dunsin <jesudunsinfaiye@gmail.com>
2026-03-24 09:32:57 +0100

    make threshold 0.65

--- app/lib/generateSeedPlaylist.ts
+++ app/lib/generateSeedPlaylist.ts
@@ -115,7 +115,7 @@ export async function generateSeedPlaylist(
 				const limit = pLimit(15);
 
         // 0.6 is a good starting point, but you can tune this
-        const THRESHOLD = 0.75
+        const THRESHOLD = 0.65
 
         // we want to avoid low quality recommendations so we check each song against all seeds(selected songs) put that into an array and sort by the number of seeds that match the treshold then return the top 100
         // we also want to avoid songs that are too far in similarity to the seeds so we set a cutoff of 0.2 below the threshold to give extra grace
