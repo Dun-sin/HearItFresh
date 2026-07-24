857e8e2c5e1b172771ba5e15d82394b77825e509
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2026-07-24 11:34:45 +0100

    Apply suggestion from @Dun-sin

--- app/lib/generateSeedPlaylist.ts
+++ app/lib/generateSeedPlaylist.ts
@@ -113,7 +113,7 @@ export async function generateSeedPlaylist(
 		const albums = await getEveryAlbum(finalList, signal);
 		throwIfAborted();
 
-		const aiTracks = (await getAllTracks(albums as string[], 3, true, signal)) as any[];
+		const aiTracks = (await getAllTracks(albums as string[], 2, true, signal)) as any[];
 
 		let fallbackTracksUris: string[] = [];
 
